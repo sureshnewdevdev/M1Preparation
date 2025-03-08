310d492a2bb4919d064709b971606b539521164f
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2025-03-08 10:31:00 +0530

    g1

---
+++ M1Preparation/CollectionExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1Preparation
+{
+    internal class CollectionExample
+    {
+    }
+}
